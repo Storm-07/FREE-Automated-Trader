5018ba51090ca8670222191df29d79a112cfb871
Nick McCrea <57852809+Storm-07@users.noreply.github.com>
2023-07-21 23:51:33 -0400

    Update BuyActivator.py

--- Hotkeys - Surface/BuyActivator.py
+++ Hotkeys - Surface/BuyActivator.py
@@ -2,10 +2,10 @@ import subprocess
 
 print("test")
 # Path to the AutoHotkey executable
-ahk_exe = r'C:/Users/ewokr/OneDrive/Desktop/Hotkeys/BuyBTC.exe'
+ahk_exe = r'[PATH]'
 
 # Path to the AutoHotkey script to execute
-ahk_script = r'C:/Users/ewokr/OneDrive/Desktop/Hotkeys/BuyBTC.ahk'
+ahk_script = r'[PATH]'
 
 # Execute the AutoHotkey script
 subprocess.Popen([ahk_exe, ahk_script])
